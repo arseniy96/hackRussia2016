66aed5a22a3ba058c0a0e9be579d3e8adb2db3c9
Vladimir Borovik <bobahbdb@gmail.com>
2016-11-18 21:46:01 +0300

    Add mainReduser

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './App';
+import mainReducer from './reducers/mainReducer';
 import './index.css';
 
 ReactDOM.render(
