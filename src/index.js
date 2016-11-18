ee0ed3ff9d0e6ac1aaffd0f226ccdc235fde5191
Vladimir Borovik <bobahbdb@gmail.com>
2016-11-18 19:48:32 +0300

    Structure of folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStore(mainReducer)}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
